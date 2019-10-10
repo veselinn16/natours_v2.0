0ae0c4a80548f74d33b6cbc89e87db39a99457c3
Veselin Tonev <veselinn16@yahoo.com>
2019-10-10 10:36:47 +0300

    Implement server error handling :wrench:

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -12,21 +14,23 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  next();
-});
-
-app.use((req, res, next) => {
-  req.reqTime = new Date().toISOString();
+  req.requestTime = new Date().toISOString();
   next();
 });
 
 // mount routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
