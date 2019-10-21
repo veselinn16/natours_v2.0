947618b474a8869327608a1f4308e45beac4d226
Veselin Tonev <veselinn16@yahoo.com>
2019-10-21 18:03:58 +0300

    Protect app from malicious attacks :wrench:

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,6 +14,8 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // GLOBAL MIDDLEWARES
+// for setting security HTTP headers
+app.use(helmet());
 
 // rate limiting middleware
 const limiter = rateLimit({
@@ -21,14 +27,38 @@ const limiter = rateLimit({
 // affects routes that only start with /api
 app.use('/api', limiter);
 
-// morgan middleware
+// morgan middleware for development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// body parsing middleware, reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); // body cannot be over 10 kb
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against XSS
+app.use(xss()); // clean any user input from any malicious HTML code with JS into it
+
+// middleware preventing parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ] // allows duplication of these parameters
+  })
+); // clears up the query string in the URL
+
+// serving static files middleware
 app.use(express.static(`${__dirname}/public`));
 
+// testing middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
