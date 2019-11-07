b57d0764f00b42ad3195f9b04b0331b372d0d5c1
Veselin Tonev <veselinn16@yahoo.com>
2019-11-07 13:56:41 +0200

    Remove server topology option :wrench:

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+    useFindAndModify: false
+    // useUnifiedTopology: true
   })
   .then(() => console.log('DB connection is successful'));
 
